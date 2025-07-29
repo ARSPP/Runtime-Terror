6ca9a4bd118eab763ffec70e3ca94089dc9d9344
as5752 <Alex.Spetseris@drexel.edu>
2025-07-28 21:59:59 -0400

    Login/Account Creation

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ const express = require('express');
 const path = require('path');
 const open = require('open');
 
+const loginApp = require('./Backend/login.js');
+
 const app = express();
 const port = 3000;
 
-// Serve main.html at root
+app.use('/', loginApp);
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 });
+
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
