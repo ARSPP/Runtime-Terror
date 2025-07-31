d5084a0c632cea85ec7a2347c960ba0142be41b4
Ethan Grim <ethangrimmy33@gmail.com>
2025-07-31 15:25:27 -0400

    Add restaurants table

--- Backend/restaurants.js
+++ Backend/restaurants.js
@@ -1,23 +1,27 @@
 let express = require('express');
 let axios = require('axios');
+let { Pool } = require("pg");
 let env = require('../env.json');
 let app = express();
 
+let pool = new Pool(env);
 app.use(express.json());
 
 app.get('/restaurants', (req, res) => {
     
     let latitude = req.query.lat;
     let longitude = req.query.long;
+    let search = req.query.search
     if(!validLocation(latitude, longitude)){
         return res.status(400).json({error:"Improper lat/long location"});
     }
 
     let restaurantCategoryId = '4d4b7105d754a06374d81259';
     let apiKey = env.foursquareApiKey;
+    let fields = "name%2Cfsq_place_id%2Clocation%2Ctel%2Cemail%2Cwebsite%2Csocial_media";
+    let url = `https://places-api.foursquare.com/places/search?ll=${latitude}%2C${longitude}&fsq_category_ids=${restaurantCategoryId}&fields=${fields}&query=${search}`;
 
-    console.log(`Lat: ${latitude}, Long: ${longitude}`);
-    axios.get(`https://places-api.foursquare.com/places/search?ll=${latitude}%2C${longitude}&fsq_category_ids=${restaurantCategoryId}&sort=DISTANCE`, {
+    axios.get(url, {
         headers :{
             'X-Places-Api-Version': '2025-06-17',
             'Accept': 'application/json',
@@ -50,5 +54,38 @@ function validLocation(lat, long) {
 
     return true;
 }
+
+app.post('/save-restaurant', async (req, res) => {
+    let restInfo = req.body;
+
+    query = `INSERT INTO restaurants (id, name, email, tel, website, location, socials)
+            VALUES ($1, $2, $3, $4, $5, $6, $7)
+            ON CONFLICT (id) DO NOTHING
+            RETURNING *;`;
+    let values = [
+        restInfo.fsq_place_id,
+        restInfo.name,
+        restInfo.email,
+        restInfo.tel,
+        restInfo.website,
+        restInfo.location,
+        restInfo.social_media
+    ]
+    try{
+        let result = await pool.query(query, values);
+        if (result.rowCount === 0) {
+            return res.status(200).json({ message: "Restaurant already exists." });
+        } else {
+            return res.status(201).json({ message: "Restaurant saved.", restaurant: result.rows[0] });
+        }
+    }catch(error){
+        console.error("Error saving restaurant:", error);
+        return res.status(500).json({ error: "Failed to save restaurant." });
+    }
+        
+
+})
+
+
 
 module.exports = app;
--- frontend/review/review.js
+++ frontend/review/review.js
@@ -1,38 +1,86 @@
 let userLat = null;
-let userLong = null
+let userLong = null;
+let queryBtn = document.getElementById("queryBtn");
+let message = document.getElementById("message");
+let searchResults = document.getElementById("results");
+queryBtn.addEventListener("click", queryRestaurants);
+
+queryBtn.disabled = true;
+message.textContent = "Trying to find your location.\n Make sure you have allowed location services for this site.";
 
 navigator.geolocation.getCurrentPosition(
   (position) => {
     userLat = position.coords.latitude;
     userLong = position.coords.longitude;
+    queryBtn.disabled = false;
+    message.textContent = "";
   },
   (error) => {
-    message.textContent = "Please enable location services to search for restaurants.";
+    message.textContent =
+      "Please enable location services to search for restaurants.";
   }
 );
 
 let restaurantQueryInput = document.getElementById("restaurantQuery");
-if(!restaurantQueryInput){
-  console.log("NULLLLL");
-}
-let queryBtn = document.getElementById("queryBtn");
-let message =  document.getElementById("message");
 
-queryBtn.addEventListener('click', queryRestaurants);
-
-function queryRestaurants(){
-    if(!userLat || !userLong ){
-        message.textContent = "Please enable location services to search for restaurants.";
-        return;
-    }
-    message.textContent = '';
-    let query = restaurantQueryInput.value;
-    console.log(query);
-    fetch(`/restaurants?lat=${userLat}&long=${userLong}`)
-    .then((response)=>{return response.json();})
-    .then((data)=>{
-      console.log(data);
-    }).catch((err) =>{
-      console.log(err);
+function queryRestaurants() {
+  searchResults.innerHTML = "";
+  if (!userLat || !userLong) {
+    message.textContent =
+      "Please enable location services to search for restaurants.";
+    return;
+  }
+  message.textContent = "";
+  let query = restaurantQueryInput.value;
+  fetch(`/restaurants?lat=${userLat}&long=${userLong}&search=${query}`)
+    .then((response) => {
+      return response.json();
     })
+    .then((data) => {
+      if (!data || data.length === 0) {
+        message.textContent = "No restaurants found for your search.";
+        return;
+      }
+      message.textContent = "";
+      data.forEach((rest) => {
+        let div = document.createElement("div");
+        div.classList.add("restaurantSearchResult");
+        div.innerHTML = `
+        <h1>${rest.name}</h1>
+        <h2>${rest.location.formatted_address}</h2>`;
+
+        let btn = document.createElement("button");
+        btn.textContent = "Add to Database";
+        btn.addEventListener("click", () => {
+          addRestaurantToDB(rest);
+        });
+
+        div.appendChild(btn);
+        searchResults.appendChild(div);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      message.textContent =
+        "Sorry there was a problem searching for restaurants.";
+    });
+}
+
+function addRestaurantToDB(data) {
+  fetch("/save-restaurant", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
